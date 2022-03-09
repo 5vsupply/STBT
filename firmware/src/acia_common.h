93ee3d7c628ee5db9dde5ba4f4c0ec4e323faa15
Nick Price <nick@spun.io>
2022-03-09 12:47:17 -0800

    stubs

--- firmware/src/acia_common.h
+++ firmware/src/acia_common.h
@@ -1,1 +1,3 @@
 // this handles communication with the MC6850 ACIA (and interrupts triggered by the HD6301V1P microcontroller)
+
+void wait_for_mcu();
--- firmware/src/acia_config.h
+++ firmware/src/acia_config.h
@@ -1,1 +1,6 @@
 // this handles configuring the device based on signals from the ACIA
+
+void get_paired_devices();
+void call_scan_bluetooth();
+void call_pair_bluetooth();
+void call_unpair_bluetooth();
