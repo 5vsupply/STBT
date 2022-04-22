ad8beee386de8ee21911734381cba1b9e1391fdb
Nick Price <nick@spun.io>
2022-04-22 10:52:38 -0700

    sync

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -4,6 +4,23 @@
 #include "driver/uart.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
+#include "esp_system.h"
 #include "esp_log.h"
 
-void app_main() {}
+void hello_task(void *pvParameter)
+{
+    for(;;) {
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+    //printf("Hello world!\n");
+    //for (int i = 10; i >= 0; i--) {
+        //printf("Restarting in %d seconds...\n", i);
+    //}
+    //printf("Restarting now.\n");
+    //fflush(stdout);
+    //esp_restart();
+}
+
+void app_main() {
+    xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
+}
